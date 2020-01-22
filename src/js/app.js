51383c0d0891691f6ede477fb8c1cd5e2d9e7fc6
haanastasia <haanastasia@yandex.com>
2020-01-22 15:09:39 +0400

    add bootstrap

--- src/js/app.js
+++ src/js/app.js
@@ -8,21 +8,3 @@ function importAll(r) {
 }
 
 const images = importAll(require.context('../userfiles/', true, /\.(png|jpe?g|svg)$/));
-
-import Blog from './blog/main';
-import Request from './api-service/request';
-const AMOUNT_FEED = 3; // кол-во статей отображаемых в ленте новостей
-
-let BlogData = new Request();
-
-BlogData.connect('https://my-json-server.typicode.com/haanastasia/EPM-FE2019/posts')
-    .then(response => {
-        let news = new Blog(JSON.parse(response));
-        return news;
-    })
-    .then(news => {
-        return news.render('.blog__col', AMOUNT_FEED);
-    })
-    .catch(error => {
-        console.log(error);
-    });
--- src/js/blog/helpers.js
+++
@@ -1,5 +0,0 @@
-// вычисляем среднее арифметическое в массиве
-export function getAvg(grades) {
-    let total = grades.reduce((acc, c) => acc + c, 0) / grades.length;
-    return total / 10;
-}
