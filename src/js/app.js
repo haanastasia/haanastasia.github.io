1baf724febf523eda714ad1d76ff88d3673e72c6
Anastasia Dolgopolova <haanastasia@yandex.com>
2020-02-14 16:07:04 +0400

    add swipe

--- src/js/app.js
+++ src/js/app.js
@@ -10,3 +10,27 @@ function importAll(r) {
 const images = importAll(require.context('../userfiles/', true, /\.(png|jpe?g|svg)$/));
 
 import './catalog/quantity';
+
+
+// Import Swiper and modules
+import { Swiper, Autoplay, Navigation, Pagination, Scrollbar } from 'swiper/js/swiper.esm.js';
+
+// Install modules
+Swiper.use([Navigation, Autoplay, Pagination, Scrollbar]);
+
+// Now you can use Swiper
+var swiper = new Swiper('.swiper-container', {
+  speed: 200,
+  autoplay: {
+    delay: 2000,
+  },
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },  
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
