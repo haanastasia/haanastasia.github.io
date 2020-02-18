f99e41130df83e72c5dd42c1e56ae9f5a0ded011
haanastasia <haanastasia@yandex.com>
2020-02-18 16:52:33 +0400

    edit price

--- src/js/catalog/cart.js
+++ src/js/catalog/cart.js
@@ -1,20 +1,28 @@
+let formatter = new Intl.NumberFormat('ru');
+
 var btn = document.querySelectorAll('.product-list__btn');
 
 var addCart = function (btn) {
     btn.addEventListener('click', function () {
         this.classList.add('btn--cart-active');
         this.textContent = 'В корзине';
-        addMiniCart(this.parentNode.querySelector(".quantity__control").value);
+        addUpdateCart();
     });
 };
 
-var addMiniCart = function(count) {
-    document.querySelector(".mini-cart__count").textContent = Number(document.querySelector(".mini-cart__count").textContent) + Number(count);
+var addUpdateCart = function() {
+    var count = 0;
+    var price = 0;
+    var product = document.querySelectorAll('.btn--cart-active');
+    for (var i = 0; i < product.length; i++) {
+        count += Number(product[i].parentNode.querySelector('.quantity__control').value);
+        price += Number(product[i].parentNode.querySelector('.price__value').getAttribute('data-price')) * Number(product[i].parentNode.querySelector('.quantity__control').value);
+    }
+     
+    document.querySelector('.nav__link--cart').title = 'Сумма: ' + formatter.format(price) + ' руб.'; 
+    document.querySelector('.mini-cart__count').textContent = count; 
 }
 
 for (var i = 0; i < btn.length; i++) {
   addCart(btn[i]);
 }
-
-
- 
--- src/js/catalog/quantity.js
+++ src/js/catalog/quantity.js
@@ -4,14 +4,14 @@ var operation = document.querySelectorAll('.quantity__operation');
 
 var addOperationClickHandler = function (operation) {
     operation.addEventListener('click', function () {
-        if (this.classList.contains("quantity__plus") && this.parentNode.querySelector("input").value < 20) {
-            ++this.parentNode.querySelector("input").value;
+        if (this.classList.contains('quantity__plus') && this.parentNode.querySelector('input').value < 20) {
+            ++this.parentNode.querySelector('input').value;
         } 
-        if (this.classList.contains("quantity__minus") && this.parentNode.querySelector("input").value > 1) {
-            --this.parentNode.querySelector("input").value;
+        if (this.classList.contains('quantity__minus') && this.parentNode.querySelector('input').value > 1) {
+            --this.parentNode.querySelector('input').value;
         }
         var price = this.parentNode.previousElementSibling;
-        price.querySelector(".price__value").innerHTML = formatter.format(Number(price.querySelector(".price__value").getAttribute("data-price")) * Number(this.parentNode.querySelector("input").value));
+        price.querySelector('.price__value').innerHTML = formatter.format(Number(price.querySelector('.price__value').getAttribute('data-price')) * Number(this.parentNode.querySelector('input').value));
     });
 };
 
